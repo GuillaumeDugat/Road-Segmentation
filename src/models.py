8a23a0a053465a0d66f71655b53abcb705854cb9
Felix Yang <79040988+fyangch@users.noreply.github.com>
2022-07-24 11:37:12 +0200

    Add notebook for resnet unet

--- src/models.py
+++ src/models.py
@@ -4,7 +4,7 @@ import torchvision
 
 
 # NOTE: The final sigmoid layer is removed from the following models because we use the BCEWithLogitsLoss which already
-# includes a sigmoid layer. We use BCEWithLogitsLoss instead of BCELoss such that we can use a weighted loss function.
+# includes a sigmoid function. We use BCEWithLogitsLoss instead of BCELoss such that we can use a weighted loss function.
 
 
 class Block(nn.Module):
@@ -51,52 +51,56 @@ class UNet(nn.Module):
         return self.head(x)  # reduce to 1 channel
 
 
-class ResnetUNet(nn.Module):
-  def __init__(self, pretrained=False, freeze=False):
-      super().__init__()
-      
-      self.freeze = freeze
-      if freeze:
-          assert pretrained
-      
-      # ResNet-18, optionally pretrained in ImageNet
-      self.resnet = torchvision.models.resnet18(pretrained=pretrained)
-      if freeze:
-          for param in self.resnet.parameters():
-              param.requires_grad = False
-      
-      # Override the final layers: we want to extract the full 7x7 final feature map
-      self.resnet.avgpool = nn.Identity()
-      self.resnet.fc = nn.Identity()
-      
-      self.conv1 = nn.Conv2d(512, 256, kernel_size=3, padding=1) # 7 -> 14
-      self.conv2 = nn.Conv2d(256, 128, kernel_size=3, padding=1) # 14 -> 28
-      self.conv3 = nn.Conv2d(128, 64, kernel_size=3, padding=1) # 28 -> 56
-      self.conv4 = nn.Conv2d(64, 32, kernel_size=3, padding=1) # 56 -> 112
-      self.conv5 = nn.Conv2d(32, 16, kernel_size=3, padding=1) # 112 -> 224
-      self.conv_out = nn.Conv2d(16, 1, kernel_size=3, padding=1) # 112 -> 224
-      self.up = nn.Upsample(scale_factor=2, mode='nearest')
-      self.relu = nn.ReLU(inplace=True)
-      
-  def forward(self, x):
-      if self.freeze:
-          self.resnet.eval()
-      
-      x = self.resnet.conv1(x)
-      x = self.resnet.bn1(x)
-      x = self.resnet.relu(x)
-      x = self.resnet.maxpool(x)
+class ResNetUNet(nn.Module):
+    def __init__(self, pretrained=False, freeze=False):
+        super().__init__()
+        
+        self.freeze = freeze
+        if freeze:
+            assert pretrained
+        
+        # ResNet-18, optionally pretrained in ImageNet
+        if pretrained:
+            self.resnet = torchvision.models.resnet18(weights=torchvision.models.ResNet18_Weights.DEFAULT)
+        else:
+            self.resnet = torchvision.models.resnet18()
 
-      x1 = self.resnet.layer1(x)
-      x2 = self.resnet.layer2(x1)
-      x3 = self.resnet.layer3(x2)
-      x = self.resnet.layer4(x3)
-      
-      x = self.up(self.relu(self.conv1(x))) + x3
-      x = self.up(self.relu(self.conv2(x))) + x2
-      x = self.up(self.relu(self.conv3(x))) + x1
-      x = self.up(self.relu(self.conv4(x)))
-      x = self.up(self.relu(self.conv5(x)))
-      x = self.conv_out(x)
+        if freeze:
+            for param in self.resnet.parameters():
+                param.requires_grad = False
+        
+        # Override the final layers: we want to extract the full 7x7 final feature map
+        self.resnet.avgpool = nn.Identity()
+        self.resnet.fc = nn.Identity()
+        
+        self.conv1 = nn.Conv2d(512, 256, kernel_size=3, padding=1) # 7 -> 14
+        self.conv2 = nn.Conv2d(256, 128, kernel_size=3, padding=1) # 14 -> 28
+        self.conv3 = nn.Conv2d(128, 64, kernel_size=3, padding=1) # 28 -> 56
+        self.conv4 = nn.Conv2d(64, 32, kernel_size=3, padding=1) # 56 -> 112
+        self.conv5 = nn.Conv2d(32, 16, kernel_size=3, padding=1) # 112 -> 224
+        self.conv_out = nn.Conv2d(16, 1, kernel_size=3, padding=1) # 112 -> 224
+        self.up = nn.Upsample(scale_factor=2, mode='nearest')
+        self.relu = nn.ReLU(inplace=True)
+        
+    def forward(self, x):
+        if self.freeze:
+            self.resnet.eval()
+        
+        x = self.resnet.conv1(x)
+        x = self.resnet.bn1(x)
+        x = self.resnet.relu(x)
+        x = self.resnet.maxpool(x)
 
-      return x
+        x1 = self.resnet.layer1(x)
+        x2 = self.resnet.layer2(x1)
+        x3 = self.resnet.layer3(x2)
+        x = self.resnet.layer4(x3)
+        
+        x = self.up(self.relu(self.conv1(x))) + x3
+        x = self.up(self.relu(self.conv2(x))) + x2
+        x = self.up(self.relu(self.conv3(x))) + x1
+        x = self.up(self.relu(self.conv4(x)))
+        x = self.up(self.relu(self.conv5(x)))
+        x = self.conv_out(x)
+
+        return x
